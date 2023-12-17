37d3994bfb5f132f744125ff1d924510ccaa11bb
Dotrako <laagad94@gmail.com>
2023-12-17 16:17:52 +0100

    Grading Program

---
+++ Day 9  - Dictionaries, Nesting and the Secret Auction/Student Grading Program.py
@@ -0,0 +1,21 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# creating a student_grades empty dictionary
+student_grades = dict()
+
+# iterating in each item of our student_scores to update our stu_grades
+for key, value in student_scores.items():
+    if value >= 91 and value <= 100:
+        student_grades.update({key:"Outstanding"})
+    elif value >= 81 and value <= 90:
+        student_grades.update({key:"Exceeds Expectations"})
+    elif value >= 71 and value <= 80:
+        student_grades.update({key:"Acceptable"})
+    else:
+        student_grades.update({key:"Fail"})
+print(student_grades)
