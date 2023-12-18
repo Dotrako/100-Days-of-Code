f4a7c117408cb96618acf61161228e91e4d435d4
Dotrako <laagad94@gmail.com>
2023-12-18 15:32:42 +0100

    Initial Bidder Input and Decision Handling

---
+++ Day 9  - Dictionaries, Nesting and the Secret Auction/The Secret Auction Program (FPSBA).py
@@ -0,0 +1,6 @@
+from Auction_Art import logo
+print(logo)
+
+bidder_name = input("Please enter your name")
+money_amount = float(input("Please place your bid"))
+next_decision = input("Are They any other bidders? Yes or No").lower()
