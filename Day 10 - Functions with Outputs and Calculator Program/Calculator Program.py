668773c203229ddad3c3ec8b4f65724d15894563
Dotrako <laagad94@gmail.com>
2023-12-20 22:01:16 +0100

    Add Calculator Final Version

--- Day 10 - Functions with Outputs and Calculator Program/Calculator Program.py
+++ Day 10 - Functions with Outputs and Calculator Program/Calculator Program.py
@@ -1,5 +1,5 @@
 # Calculator Program
-
+from Art import logo
 # Addition
 def addition(n1, n2):
     return n1 + n2
@@ -16,23 +16,44 @@ def multiply(n1, n2):
 def division(n1,n2):
     return n1 / n2
 
-operations = {"+": addition, 
-              "-": substract, 
-              "*": multiply,
-              "/": division,
-}
+# var = input("Enter y to continue and n to finish").lower()
 
-num1 = float(input("Enter the first number"))
+def calculator():
+    print(logo)
+    operations = {"+": addition, 
+                "-": substract, 
+                "*": multiply,
+                "/": division,
+    }
 
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Please pick an opperation symbol (+, -, * or /)")
-num2 = float(input("Enter the second number"))
+    num1 = float(input("Enter the first number"))
 
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+    for symbol in operations:
+        print(symbol)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-    
-# for value in calculator.values():
-#     print(value)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Please pick an opperation symbol (+, -, * or /)")
+        num2 = float(input("Enter the second number"))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue with {answer}, or type 'n' to exit:") == "y":
+            num1 = answer 
+        else:
+            should_continue = False
+            calculator()  
+calculator()
+
+    # operation_symbol = input("Please pick an opperation symbol (+, -, * or /)")
+    # num3 = float(input("Enter the next number"))
+    # calculation_function = operations[operation_symbol]
+    # # answer2 = calculation_function(calculation_function(num1, num2), num3)
+    # answer2 = calculation_function(answer1, num3)
+
+    # print(f"{answer1} {operation_symbol} {num3} = {answer2}")
+    # var = input("Enter y to continue and n to finish").lower()
+
+    # # for value in calculator.values():
+    # #     print(value)
