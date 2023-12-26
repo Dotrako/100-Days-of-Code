e9070007b25a9d8c35da8516f5506e4f1d094163
Dotrako <laagad94@gmail.com>
2023-12-26 16:33:34 +0100

    Initialize game data and logo display

---
+++ Day 14 - Higher Lower Game Project/Lower Higher Program.py
@@ -0,0 +1,11 @@
+# import necessary library and modules
+import random
+from Art import logo, vs
+from game_data import data
+
+# testing logo and data storing module
+print(logo)
+print(vs)
+print(data)
+
+# 
