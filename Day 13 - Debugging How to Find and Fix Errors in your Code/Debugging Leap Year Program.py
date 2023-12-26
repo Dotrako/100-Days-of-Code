d048fbfcc367be8706a20920bbe2c92be92a4007
Dotrako <laagad94@gmail.com>
2023-12-26 14:09:58 +0100

    Leap Year Program Debugging

---
+++ Day 13 - Debugging How to Find and Fix Errors in your Code/Debugging Leap Year Program.py
@@ -0,0 +1,27 @@
+# # Which year do you want to check?
+# year = input()
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+# Which year do you want to check?
+year = int(input("Enter the Year you want")) # TypeError without int() conversion
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
