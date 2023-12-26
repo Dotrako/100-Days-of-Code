2578c2d90c680952908f8605a1b1fc15ca1d5763
Dotrako <laagad94@gmail.com>
2023-12-26 15:48:10 +0100

    FizzBazz Program Debug

---
+++ Day 13 - Debugging How to Find and Fix Errors in your Code/Debugging FizzBazz Program.py
@@ -0,0 +1,21 @@
+# target = int(input())
+# for number in range(1, target + 1):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+
+target = int(input("Enter your number"))
+for number in range(1, target + 1):
+  if number % 3 == 0 or number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
