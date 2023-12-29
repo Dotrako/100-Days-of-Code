b1d60bb470ead849b084399ce04eee92e369fa0a
Dotrako <laagad94@gmail.com>
2023-12-29 14:16:06 +0100

    Added comments for clarity and structure to the code for the follower comparison game.

--- Day 14 - Higher Lower Game Project/Lower Higher Program.py
+++ Day 14 - Higher Lower Game Project/Lower Higher Program.py
@@ -1,36 +1,82 @@
-# import useful library and modules
-import random as rd
+# Importing necessary libraries and modules
+import random
+import os
 from Art import logo, vs
 from game_data import data
 
-first_subject = rd.choice(data)
-second_subject = rd.choice(data)
+# Function to get data from a random account
+def get_random_account():
+    """Get data from random account"""
+    return random.choice(data)
 
-start = input("Press Y to play or N to leave the game: ").lower()
+# Function to format account data into a printable format
+def format_data(account):
+    """Format account into printable format: name, description, and country"""
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
 
-def game_rules(subject_A, subject_B):
-    print(f"Choose A for {subject_A['name']}")
-    print(f"Choose B for {subject_B['name']}")
-
-def game_player_process(player_choice, subject_A, subject_B):
-    if player_choice == "a":
-        return first_subject["follower_count"]
+# Function to check if the user's guess is correct
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess 
+    and returns True if they got it right.
+    Or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess == "a"
     else:
-        return second_subject["follower_count"]
+        return guess == "b"
 
-def game_cpu_process(player_choice, subject_A, subject_B):
-    if player_choice == first_subject["follower_count"]:
-        return second_subject["follower_count"]
+# Function to clear the console output
+def clear():
+    """Clears the console output based on the operating system."""
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # For Linux and macOS
     else:
-        return first_subject["follower_count"]
+        _ = os.system('clear')
 
-if start == "y":
-    game_rules(first_subject, second_subject)
-    choice = input("Please enter your choice: ").lower()
-    choice1 = input("Please enter your choice: ").lower()
+# Main game function
+def game():
+    # Display the game logo
+    print(logo)
+    score = 0
+    game_should_continue = True
     
-    player_result = game_player_process(choice, first_subject, second_subject)
-    print(f"Player choice result: {player_result}")
-    
-    cpu_result = game_cpu_process(choice1, first_subject, second_subject)
-    print(f"CPU choice result: {cpu_result}")
+    # Get initial random accounts
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while game_should_continue:
+        # Update accounts for comparison
+        account_a = account_b
+        account_b = get_random_account()
+
+        # Ensure different accounts are chosen for comparison
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        # Display account details for comparison
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+        
+        # Get user's guess and compare follower counts
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        # Clear the console and display game status
+        clear()
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")
+
+# Start the game
+game()
