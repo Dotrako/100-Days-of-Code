618c3a4e97c28a9a41dac167da68db982bb85d7f
Dotrako <laagad94@gmail.com>
2023-12-29 17:00:34 +0100

    Add initial Menu and Resources definitions for Coffee Machine Program

---
+++ Day 15 - Local Development Environment Setup & the Coffee Machine/The Coffee Machine Project.py
@@ -0,0 +1,15 @@
+# importing useful libraries and modules
+from Coffee_Data import MENU, resources
+print(MENU)
+print(resources)
+
+# inputing the customer's choice (Espresso, Latte, Cappucino)
+customer_choice = input("Please Enter your wanted coffe:\nE for Esspresso \nL for Latte \n C for Cappuccino")
+
+# inputting the customer's money (Quarters, Dimes, Nickles, Pennies)
+
+# checking if the money is enough 
+
+# delivering th chosen type of coffee
+
+
