a3717ecdf6d212236a4f9073aa2fe7b936b1196b
Dotrako <laagad94@gmail.com>
2024-01-01 23:55:32 +0100

    Implement turtle movement and direction controls for Timmy

--- Day 16 - Object Oriented Programming (OOP)/Day 16 start.py
+++ Day 16 - Object Oriented Programming (OOP)/Day 16 start.py
@@ -13,6 +13,12 @@ timmy.shape("turtle")
 # turtle color
 timmy.color("red", "green")
 
+# moving my freind timmy 
+timmy.forward(100) # fd() can also work
+timmy.left(100) 
+timmy.back(200) # bk()
+timmy.right(200)
+
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
