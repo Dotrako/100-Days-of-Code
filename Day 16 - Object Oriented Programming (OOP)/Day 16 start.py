2262c52a82e325e3d5fe7e12385f0c272bf958fe
Dotrako <laagad94@gmail.com>
2024-01-01 23:58:41 +0100

    Extend turtle functionality to draw shapes and squares with Timmy (to demonstrsate the use of methodes and attributes)

--- Day 16 - Object Oriented Programming (OOP)/Day 16 start.py
+++ Day 16 - Object Oriented Programming (OOP)/Day 16 start.py
@@ -13,12 +13,16 @@ timmy.shape("turtle")
 # turtle color
 timmy.color("red", "green")
 
-# moving my freind timmy 
-timmy.forward(100) # fd() can also work
-timmy.left(100) 
-timmy.back(200) # bk()
-timmy.right(200)
+# # moving my freind timmy 
+# timmy.forward(100) # fd() can also work
+# timmy.left(100) 
+# timmy.back(200) # bk()
+# timmy.right(200)
 
+# Draw a square with Timmy
+for _ in range(4):
+    timmy.forward(100)
+    timmy.right(90)
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
