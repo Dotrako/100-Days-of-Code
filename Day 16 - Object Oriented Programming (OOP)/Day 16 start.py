950bfa8c1b49de53acbdf7098f58bebb9a4bee4b
Dotrako <laagad94@gmail.com>
2024-01-01 19:35:22 +0100

    Enhance turtle drawing with color and screen interaction

--- Day 16 - Object Oriented Programming (OOP)/Day 16 start.py
+++ Day 16 - Object Oriented Programming (OOP)/Day 16 start.py
@@ -4,8 +4,15 @@
 # import anothermodule 
 # print(anothermodule.x)
 
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
 
+# turtle color
+timmy.color("red", "green")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
