467d760d9fe91baca036f8c203b459921b3990a9
Dotrako <laagad94@gmail.com>
2024-01-04 18:17:06 +0100

    Refactor Quiz Initialization and Question Retrieval

--- Day 17 - The Quiz Project & the Benefits of OOP/Quiz Project/main.py
+++ Day 17 - The Quiz Project & the Benefits of OOP/Quiz Project/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 # Initialize an empty list to store Question objects
 question_bank = list()
@@ -10,7 +11,8 @@ for question in question_data:
     question_answer = question["answer"]
     new_quesstion = Question(question_text, question_answer)    
     question_bank.append(new_quesstion)
-    
-# Print the text and answer of the second question in the question_bank (index starts at 0)
-print(question_bank[1].text)
-print(question_bank[1].answer)
+
+# Create a QuizBrain object with the question_bank list
+quiz = QuizBrain(question_bank)
+# Call the next_question method to display the first question
+quiz.next_question()
--- Day 17 - The Quiz Project & the Benefits of OOP/Quiz Project/question_model.py
+++ Day 17 - The Quiz Project & the Benefits of OOP/Quiz Project/question_model.py
@@ -1,4 +1,3 @@
-
 class Question:
     def __init__(self, q_text, q_answer):
         self.text = q_text 
