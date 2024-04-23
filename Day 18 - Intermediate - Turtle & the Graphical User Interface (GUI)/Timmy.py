6f41c95b65a16097a2cd9838eee2e9fa3cb46631
Dotrako <laagad94@gmail.com>
2024-04-23 22:55:20 +0100

    Add turtle movement and shape customization:
    - Set the turtle shape to "turtle"
    - Set the turtle's pen color to red and fill color to green
    - Move the turtle forward by 90 units
    - Turn the turtle right by 90 degrees
    - Move the turtle backward by 90 units
    - Turn the turtle left by 90 degrees
    - Move the turtle backward by 90 units
    - Turn the turtle left by 90 degrees
    - Move the turtle backward by 90 units
    - Implement a loop to repeat the movement pattern 4 times:
    
        - Move the turtle forward by 90 units
        - Turn the turtle left by 90 degrees

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
@@ -2,10 +2,23 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red", "green")
+timmy_the_turtle.forward(90)
+timmy_the_turtle.right(90)
+timmy_the_turtle.back(90)
+timmy_the_turtle.left(90)
+timmy_the_turtle.back(90)
+timmy_the_turtle.left(90)
+timmy_the_turtle.back(90)
+
+# or with a loop
+
+for i in range(4):
+    timmy_the_turtle.forward(90)
+    timmy_the_turtle.left(90)
 
 screen_object = Screen()
 screen_object.exitonclick()
-
-
 
 
