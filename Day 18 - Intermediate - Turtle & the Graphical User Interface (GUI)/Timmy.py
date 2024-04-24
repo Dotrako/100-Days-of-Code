4502b576503fc5a55ed0c6416d80e064319e08aa
Dotrako <laagad94@gmail.com>
2024-04-24 21:24:54 +0100

    Draw a dashed line using turtle graphics
    - Create a Turtle object named 'timmy'
    - Set the turtle shape to "turtle"
    - Set the turtle color to "green"
    - Implement a loop that repeats 5 times:
      - Move the turtle forward by 10 units with the pen down
      - Lift the pen up using 'penup()'
      - Move the turtle forward by 10 units with the pen up
      - Put the pen down using 'pendown()'
    - Create a Screen object named 'display'
    - Set the screen to close when clicked using 'exitonclick()'
    
    This commit creates a script that uses the Turtle library to draw a dashed line. The turtle moves forward, alternating between drawing a line segment and moving forward without drawing.

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
 # timmy_the_turtle = Turtle()
 
@@ -22,9 +22,65 @@ from turtle import Turtle, Screen
 # screen_object.exitonclick()
 
 
-import heroes
-print(heroes.gen())
+# import heroes
+# print(heroes.gen())
 
-from heroes import *
+# from heroes import *
 
-from heroes import gen
+# from heroes import gen
+
+
+# Drawing a Dashed Line Challenge
+# importing necessary libraries
+
+# from turtle import Turtle as T
+# from turtle import Screen as S
+
+# tim = T()
+# tim.shape("Turtle")
+# tim.color("red", "green")
+
+
+# screen = S()
+# screen.exitonclick()
+
+
+# from turtle import Turtle , Screen
+
+
+# my_pet = Turtle()
+# my_pet.shape("turtle")
+# my_pet.color("Red","Green")
+
+
+# for i in range(4):
+#     my_pet.forward(100)
+#     my_pet.left(90)
+
+
+# display = Screen()
+# display.exitonclick()
+
+
+
+# we want to write a script for  a turtle that will right one _ and then jump to do a shape like this: _ _ _ _ _ _ _ _
+
+from turtle import Turtle, Screen
+
+# Creating the turtle using the Turtle library method
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("green")
+
+for i in range(5):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+
+
+
+display = Screen()
+display.exitonclick()
+
