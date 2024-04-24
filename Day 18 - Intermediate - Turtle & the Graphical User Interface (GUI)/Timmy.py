dec66c1af261918d3f258be58c8a70267d296b31
Dotrako <laagad94@gmail.com>
2024-04-24 22:22:13 +0100

    feat: Draw multiple shapes with different colors and increasing sides
    - Create a Turtle object named 'timmy'
    - Set the turtle shape to "turtle"
    - Define a list of colors named 'colours'
    - Implement a function 'draw_shape' that takes the number of sides as a parameter:
      - Calculate the angle for each turn based on the number of sides
      - Use a loop to repeat the drawing process for the given number of sides:
        - Move the turtle forward by 100 units
        - Turn the turtle right by the calculated angle
    - Implement a loop that iterates from 3 to 10 (inclusive):
      - Set the turtle color to a randomly chosen color from the 'colours' list
      - Call the 'draw_shape' function with the current number of sides
    - Create a Screen object named 'display'
    - Set the screen to close when clicked using 'exitonclick()'
    
    This commit adds functionality to draw multiple shapes with increasing number of sides and random colors. The code does the following:

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
@@ -66,21 +66,45 @@
 # we want to write a script for  a turtle that will right one _ and then jump to do a shape like this: _ _ _ _ _ _ _ _
 
 from turtle import Turtle, Screen
+import random
 
 # Creating the turtle using the Turtle library method
 
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("green")
+
+# for i in range(5):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
+
+
+
+# display = Screen()
+# display.exitonclick()
+
+
+# Creating a shaped hexagon
+
 timmy = Turtle()
 timmy.shape("turtle")
-timmy.color("green")
 
-for i in range(5):
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
-    timmy.pendown()
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen"]
 
 
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for i in range(num_sides):
+        timmy.forward(100)
+        timmy.right(angle)
+    
+
+for shape_side_n in range(3,11):
+    timmy.color(random.choice(colours))
+    draw_shape(shape_side_n)
 
 display = Screen()
 display.exitonclick()
-
