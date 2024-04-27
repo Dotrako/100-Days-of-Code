f8409a9b1ffa8bdca00111ac1362e411c1f41282
Dotrako <laagad94@gmail.com>
2024-04-27 20:22:01 +0100

    Fix and refactor random walk art code
    - Fix the 'random_color' function to return the generated color tuple
    - Remove unnecessary variable assignment for 'random_color'
    - Use the 'random_color()' function directly when setting the turtle color
    - Improve code formatting and readability
    
    This commit fixes and refactors the code for generating random walk art using turtle graphics:

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
@@ -113,22 +113,23 @@ import turtle as t
 import random
 
 tim = t.Turtle()
-tim.shape("Turtle")
+tim.shape("turtle")
 t.colormode(255)
 
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
-    
-coulours = (random_color())
+    random_color = (r, g, b)
+    return random_color
+
 
 directions = [0, 90 , 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 for i in range(200):
-    tim.color(random.choice(coulours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
