0b6bcf0edaf2868e2d665c323910ad515a8f6640
Dotrako <laagad94@gmail.com>
2024-04-27 20:11:36 +0100

    Random walk art with turtle graphics
    - Import turtle and random modules
    - Create Turtle object 'tim' with "Turtle" shape
    - Set color mode to RGB and implement 'random_color' function
    - Define 'directions' list for different angles
    - Set pen size to 15 and turtle speed to "fastest"
    - Loop 200 times: set random color, move forward, change direction
    - Create Screen object and set to close on click
    
    This commit generates random walk art using turtle graphics, with random colors and directions.

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
@@ -88,23 +88,50 @@ import random
 
 # Creating a shaped hexagon
 
-timmy = Turtle()
-timmy.shape("turtle")
+# timmy = Turtle()
+# timmy.shape("turtle")
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen"]
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen"]
 
 
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for i in range(num_sides):
-        timmy.forward(100)
-        timmy.right(angle)
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for i in range(num_sides):
+#         timmy.forward(100)
+#         timmy.right(angle)
     
 
-for shape_side_n in range(3,11):
-    timmy.color(random.choice(colours))
-    draw_shape(shape_side_n)
+# for shape_side_n in range(3,11):
+#     timmy.color(random.choice(colours))
+#     draw_shape(shape_side_n)
 
-display = Screen()
+# display = Screen()
+# display.exitonclick()
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+tim.shape("Turtle")
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    
+coulours = (random_color())
+
+directions = [0, 90 , 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for i in range(200):
+    tim.color(random.choice(coulours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+
+display = t.Screen()
 display.exitonclick()
