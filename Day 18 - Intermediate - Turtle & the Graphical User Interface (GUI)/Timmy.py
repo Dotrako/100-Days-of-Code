9605f11064a9dcea031ef1f769bb144f7757bdc6
Dotrako <laagad94@gmail.com>
2024-04-27 21:20:15 +0100

    feat: Enhance spirograph art with customizable gap size
    - Import turtle and random modules
    - Create Turtle object 'tim' with "turtle" shape
    - Set color mode to RGB
    - Implement 'random_color' function to generate random RGB color tuples
    - Set turtle speed to "fastest"
    - Implement 'draw_spirograph' function that takes 'size_of_gap' as a parameter:
      - Loop 'int(360 / size_of_gap)' times:
        - Set turtle color using 'random_color()'
        - Draw a circle with radius 100
        - Adjust the heading by adding 'size_of_gap' to the current heading
    - Call 'draw_spirograph' function with gap size of 5
    - Create Screen object and set to close on click

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Timmy.py
@@ -180,12 +180,13 @@ def random_color():
 
 tim.speed("fastest")
 
-for _ in range(100):
-    tim.color(random_color())
-    tim.circle(100)
-    tim.setheading(tim.heading() + 10)
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
 
-
+draw_spirograph(5)
 
 display = t.Screen()
 display.exitonclick()
