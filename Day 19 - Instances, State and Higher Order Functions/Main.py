c42d98d0f9d5d7d0f39595f869f833d0e6a280e6
Dotrako <laagad94@gmail.com>
2024-04-29 22:45:34 +0100

    Enhance turtle movement with additional keyboard controls
    - Add functions for backward movement (backwards) and counter-clockwise rotation (counter_clockwise)
    - Bind the "s" key to the backwards function to move the turtle backward by 10 units
    - Bind the "a" key to the counter_clockwise function to rotate the turtle counter-clockwise by 10 degrees
    - Update the screen to listen for the new keyboard events

--- Day 19 - Instances, State and Higher Order Functions/Main.py
+++ Day 19 - Instances, State and Higher Order Functions/Main.py
@@ -15,10 +15,19 @@ screen = Screen()
 def move_forwrad():
     tim.forward(10)
 
+def backwards():
+    tim.backward(10)
+
+def counter_clockwise():
+    tim.left(10)
+    
 
 
 
 
 screen.listen()
 screen.onkey(key = "space", fun = move_forwrad)
+screen.onkey(key = "s", fun=backwards)
+screen.onkey(key = "a", fun = counter_clockwise)
+
 screen.exitonclick()
