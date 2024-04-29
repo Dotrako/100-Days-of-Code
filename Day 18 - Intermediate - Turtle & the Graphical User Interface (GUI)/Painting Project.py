c69118bf4db648917493623bf91dcd62c8318954
Dotrako <laagad94@gmail.com>
2024-04-29 22:21:50 +0100

    Create a Hirst Painting using Turtle Graphics
    - Set up the turtle object and position it
    - Iterate over the dots and draw them with random colors
    - Handle the end of each row by repositioning the turtle
    - Create a screen object and set it to exit on click

---
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Painting Project.py
@@ -0,0 +1,53 @@
+# The Hirst Painting project:
+
+# Extracting RGB Values from an image 
+
+import colorgram
+import turtle as T
+import random
+rgb_colors = []
+colors = colorgram.extract("image.jpg", 30)
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+
+
+colour_list = [(202, 164, 110), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (10168, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]    
+
+T.colormode(255)
+tim = T.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(colour_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+
+
+
+
+
+
+screen = T.Screen()
+screen.exitonclick()
